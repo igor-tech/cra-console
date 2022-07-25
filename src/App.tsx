066ad9862ccfad8fc8e8111ab46a15807d36f265
igorek <igor20513@gmail.com>
2022-07-25 16:35:34 +0500

    Dev branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>11</p>
-        <p>2</p>
-        <p>3</p>
-      </header>
+      <div>App</div>
+      <div>Dev branch commit 1</div>
     </div>
   );
 }
