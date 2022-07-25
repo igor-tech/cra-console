324af89eedd4e1d176e9234d287a1e22da101250
igorek <igor20513@gmail.com>
2022-07-25 16:47:41 +0500

    develop/popup branch commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <div>Dev branch commit 2</div>
       <div>Dev branch commit 3</div>
       <div>develop/popup branch commit 1</div>
+      <div>develop/popup branch commit 2</div>
 
     </div>
   );
